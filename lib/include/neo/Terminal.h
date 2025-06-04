04367bf1bfcb510ec7052788bc515da6f6748ae2
nirevalis <piotrchudy1111+nirevalis@gmail.com>
2025-06-04 22:48:27 +0200

    Prompt system

--- lib/include/neo/Terminal.h
+++ lib/include/neo/Terminal.h
@@ -4,6 +4,8 @@
 #include <neo/math/Color.h>
 #include <string_view>
 #include <format>
+#include <utility>
+#include <vector>
 #include <stack>
 
 namespace Neo
@@ -18,6 +20,105 @@ namespace Neo
         TerminalStyle(const Color& foreground, const Color& background) : Foreground(foreground), Background(background) {}
     };
 
+    enum class UserInputDataType : uint8
+    {
+        String,
+        Int32,
+        Boolean
+    };
+
+    class Questions
+    {
+    private:
+        friend class Terminal;
+
+        struct Answer
+        {
+            UserInputDataType Type;
+
+            union ValueUnion
+            {
+                char* Text;
+                struct { int32_t NumberValue; int32_t _i32fill; };
+                struct { bool BoolValue; uint8_t _i8fill[7]; };
+
+                ValueUnion() : Text(nullptr) {}
+                ~ValueUnion() {}
+            } Value;
+
+            explicit Answer(UserInputDataType type) : Type(type)
+            {
+                if (Type == UserInputDataType::String)
+                    Value.Text = nullptr;
+            }
+
+            ~Answer()
+            {
+                if (Type == UserInputDataType::String && Value.Text)
+                    delete[] Value.Text;
+            }
+
+            Answer(const Answer&) = delete;
+            Answer& operator=(const Answer&) = delete;
+
+            Answer(Answer&& other) noexcept : Type(other.Type), Value{}
+            {
+                if (Type == UserInputDataType::String)
+                {
+                    Value.Text = other.Value.Text;
+                    other.Value.Text = nullptr;
+                }
+                else if (Type == UserInputDataType::Int32)
+                {
+                    Value.NumberValue = other.Value.NumberValue;
+                }
+                else if (Type == UserInputDataType::Boolean)
+                {
+                    Value.BoolValue = other.Value.BoolValue;
+                }
+            }
+
+            Answer& operator=(Answer&& other) noexcept
+            {
+                if (this != &other)
+                {
+                    if (Type == UserInputDataType::String && Value.Text)
+                        delete[] Value.Text;
+
+                    Type = other.Type;
+
+                    if (Type == UserInputDataType::String)
+                    {
+                        Value.Text = other.Value.Text;
+                        other.Value.Text = nullptr;
+                    }
+                    else if (Type == UserInputDataType::Int32)
+                    {
+                        Value.NumberValue = other.Value.NumberValue;
+                    }
+                    else if (Type == UserInputDataType::Boolean)
+                    {
+                        Value.BoolValue = other.Value.BoolValue;
+                    }
+                }
+                return *this;
+            }
+        };
+
+        struct Question
+        {
+            std::string Text;
+            UserInputDataType Type;
+            Answer Answer;
+
+            Question(std::string  text, UserInputDataType type) : Text(std::move(text)), Type(type), Answer(type) {}
+        };
+
+        std::vector<Question> m_Questions;
+    public:
+        void Add(int32 index, const std::string& text, UserInputDataType type = UserInputDataType::String);
+        [[nodiscard]] const Answer& GetAnswer(int32 index) const;
+    };
 
     class Terminal
     {
@@ -28,12 +129,18 @@ namespace Neo
         static void setup();
         static void printStringColored(const std::string_view& str, const std::string& end = "\n");
 
+        static int32 promptInt32();
     public:
         Terminal() = delete;
         ~Terminal() = delete;
 
         static void PushStyle(const TerminalStyle& style);
         static void PopStyle(uint8 count = 1);
+
+        static void Pause();
+        static void Clear();
+
+        static void Ask(Questions& questions);
 
         template <typename... Args>
         inline static void Print(std::format_string<Args...> format, Args&&... args)
--- lib/src/neo/Terminal.cpp
+++ lib/src/neo/Terminal.cpp
@@ -1,15 +1,24 @@
 #include <neo/Terminal.h>
 #include <cstdio>
+#include <iostream>
+#include <string>
 
 #ifdef WIN32
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <Windows.h>
+#include <conio.h>
 #endif
 
 namespace Neo
 {
 #define ENSURE_INITIALIZED() if(!m_Initialized) setup();
+
+    void Questions::Add(int32 index, const std::string &text, UserInputDataType type)
+    {
+        m_Questions.emplace(m_Questions.begin() + index, text, type);
+    }
+
     void Terminal::setup()
     {
         m_StyleStack.emplace(Color::White, Color::Black);
@@ -46,5 +55,85 @@ namespace Neo
 
         applyStyle(m_StyleStack.top());
         printf("%s%s\x1b[0m", str.data(), end.data());
+    }
+
+    void Terminal::Clear()
+    {
+#if defined(WIN32) || defined(unix) || defined(__APPLE__)
+        printf("\x1B[2J\x1B[H");
+#endif
+    }
+
+    void Terminal::Pause()
+    {
+#ifdef WIN32
+        _getch();
+#endif
+    }
+
+    void Terminal::Ask(Questions& questions)
+    {
+        for (Questions::Question& question : questions.m_Questions)
+        {
+            Questions::Answer& answer = question.Answer;
+
+            std::string additionalInfo;
+            if (question.Type == UserInputDataType::Boolean)
+            {
+                additionalInfo = " (y/n) (default: y)";
+            }
+
+            PushStyle(TerminalStyle{Color::Green, Color::Black});
+            Print("(?) {}{}", question.Text, additionalInfo);
+            PopStyle();
+
+            switch (question.Type)
+            {
+                case UserInputDataType::String:
+                {
+                    std::string s;
+                    std::getline(std::cin, s);
+
+                    //Oh man i dont really like that implementation...
+                    answer.Value.Text = new char[s.size() + 1];
+                    answer.Value.Text[s.size()] = '\0';
+                    memcpy(answer.Value.Text, s.data(), s.size());
+                    break;
+                }
+                case UserInputDataType::Int32:
+                {
+                    while (true)
+                    {
+                        std::cin >> answer.Value.NumberValue;
+
+                        if (std::cin.fail()) {
+                            std::cin.clear();
+                            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                            PushStyle(TerminalStyle{Color::Red, Color::Black});
+                            Print("Value is not a number! Please enter value again.");
+                            PopStyle();
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case UserInputDataType::Boolean:
+                {
+                    std::string s;
+                    std::cin >> s;
+
+                    answer.Value.BoolValue = s != "n";
+                    break;
+                }
+            }
+        }
+    }
+
+    const Questions::Answer& Questions::GetAnswer(int32 index) const
+    {
+        return m_Questions[index].Answer;
     }
 }
